e195497fede1fb955d6e439e612565db9409e014
Yuvi Panda <yuvipanda@gmail.com>
2010-02-04 15:06:43 +0530

    Modified model_package tests to remove tests for contrib.site functionality

--- tests/modeltests/model_package/models/article.py
+++ tests/modeltests/model_package/models/article.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.sites.models import Site
 
 class Article(models.Model):
-    sites = models.ManyToManyField(Site)
     headline = models.CharField(max_length=100)
     publications = models.ManyToManyField("model_package.Publication", null=True, blank=True,)
 
--- tests/modeltests/model_package/tests.py
+++ tests/modeltests/model_package/tests.py
@@ -10,31 +10,22 @@ class Advertisment(models.Model):
 __test__ = {'API_TESTS': """
 >>> from models.publication import Publication
 >>> from models.article import Article
->>> from django.contrib.auth.views import Site
 
 >>> p = Publication(title="FooBar")
 >>> p.save()
 >>> p
 <Publication: Publication object>
 
->>> from django.contrib.sites.models import Site
->>> current_site = Site.objects.get_current()
->>> current_site
-<Site: example.com>
-
 # Regression for #12168: models split into subpackages still get M2M tables
 
 >>> a = Article(headline="a foo headline")
 >>> a.save()
 >>> a.publications.add(p)
->>> a.sites.add(current_site)
 
 >>> a = Article.objects.get(id=1)
 >>> a
 <Article: Article object>
 >>> a.id
-1
->>> a.sites.count()
 1
 
 # Regression for #12245 - Models can exist in the test package, too
